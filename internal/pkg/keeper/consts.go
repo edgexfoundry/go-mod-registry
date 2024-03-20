d2ed7e792f9cae09deddb7cc4f155098de030ee2
Jack Chen <jack@iotechsys.com>
2024-03-20 18:48:03 +0800

    feat: Add Core Keeper registry client
    closes #134
    
    Signed-off-by: Jack Chen <jack@iotechsys.com>

---
+++ internal/pkg/keeper/consts.go
@@ -0,0 +1,12 @@
+//
+// Copyright (C) 2024 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package keeper
+
+import "github.com/edgexfoundry/go-mod-core-contracts/v3/common"
+
+const (
+	ApiRegistrationByServiceIdRoute = common.ApiRegisterRoute + "/" + common.ServiceId + "/"
+)
